0393851f628e7a029251fda550ebe8b4deae4d1c
suletetes <sulea841@gmail.com>
2022-08-10 01:59:55 +0000

    patch 2

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "suletetesuleiman.eu-gb.mybluemix.net "]
+ALLOWED_HOSTS = ["localhost", "suletetefirst.eu-gb.mybluemix.net "]
 
 
 # Application definition
