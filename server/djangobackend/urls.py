82d302955c1e1fc1117442916c5fd054d1089e28
suletetes <sulea841@gmail.com>
2022-08-10 00:42:26 +0000

    testing urls.py

--- server/djangobackend/urls.py
+++ server/djangobackend/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# test
